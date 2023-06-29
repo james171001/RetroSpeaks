24cd6b9149baad3798af260a6033418547f1cf95
RaineEdianel <raineedianel21@gmail.com>
2023-06-29 17:51:14 +0800

    Added Login & Register Component
    Routes for these components are working

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -3,18 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './app/login/login.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-import { ForgotPasswordEmailVerificationComponent } from './forgot-password-email-verification/forgot-password-email-verification.component';
-import { ForgotPasswordPasswordChangeComponent } from './forgot-password-password-change/forgot-password-password-change.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    ForgotPasswordEmailVerificationComponent,
-    ForgotPasswordPasswordChangeComponent
   ],
   imports: [
     BrowserModule,
--- Angular/src/app/register/register.component.ts
+++ Angular/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  constructor(private router: Router) {}
+
+  goToLogin() {
+    this.router.navigate(['/login']);
+  }
 }
