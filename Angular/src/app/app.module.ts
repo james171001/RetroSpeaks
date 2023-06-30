636c0157110a51151fd54a01b2fb145a3b8eab37
uhleesuuuh <espiritu.alisa@gmail.com>
2023-06-30 14:12:17 +0800

    Added sidebar component

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
